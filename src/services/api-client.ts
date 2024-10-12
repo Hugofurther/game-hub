39831a0b828a39c0f3fdc585c8690d1e7e0ab4e6
Hugo Marcotte <hugofurther@gmail.com>
2024-10-12 12:59:33 -0700

    Create a Custom Hook for Fetching Games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,3 +1,5 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
+
+export {CanceledError};
 
 export default axios.create({baseURL: "https://api.rawg.io/api", params: {key: "d14daf4e30b64cb8bda1330f867251ab"}});
